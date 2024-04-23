d58a1b63ecac22b2e9db1a5f18c3417eb337a386
Nelson Malbone <nhmalbone@icloud.com>
2024-04-23 15:50:52 -0400

    added a footer to each page

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='p', unit='mm', format='a4')
+# page should not break automatically
+pdf.set_auto_page_break(auto=False, margin=0)
 # loading csv in a pandas data frame
 df = pd.read_csv('topics.csv')
 # print(df) to make sure data got loaded correctly
 for index, row in df.iterrows():
-    pdf.add_page()
+    pdf.add_page()  # parent page
     pdf.set_font(family='Times', style='B', size=24)
     # border= 'B' does the same thing
     pdf.cell(w=0, h=24, txt=row['Topic'], align='L', ln=1)
     # this is a painful way of adding a line you just have more control of line placement
     pdf.line(10, 28, 200, 28)
 
+
+    pdf.ln(250)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+
+
     for i in range(row['Pages'] - 1):
-        pdf.add_page()
+        pdf.add_page()  # child page
         # third page came from the first pdf.add_page inside the first for loop.
         # to solve just add a - 1 like above
 
