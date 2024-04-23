318d999ec0dbb139b0801b3519c887db703f3ac9
Nelson Malbone <nhmalbone@icloud.com>
2024-04-23 15:15:21 -0400

    added the title to each page using a for loop. and add the text format to go with it

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from fpdf import FPDF
 import pandas as pd
 
-pdf  = FPDF(orientation='p', unit='mm', format='a4')
+pdf = FPDF(orientation='p', unit='mm', format='a4')
 # loading csv in a pandas data frame
 df = pd.read_csv('topics.csv')
 # print(df) to make sure data got loaded correctly
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family='Times', style='B', size=24)
+    # border= 'B' does the same thing
+    pdf.cell(w=0, h=24, txt=row['Topic'], align='L', ln=1)
+    # this is a painful way of adding a line you just have more control of line placement
+    pdf.line(10, 28, 200, 28)
 
-pdf.add_page()
+
+
 # leave blank till output is made
-
-pdf.set_font(family='Times', size=12)
-pdf.cell(w=0, h=12, txt="Hello", align='L', ln=1, border=1)
-pdf.set_font(family='Times', style='B', size=12)
-pdf.cell(w=0, h=12, txt="hi there", align='L', ln=1, border=1)
+# just learning about the FPDF
+# pdf.add_page()
+# pdf.set_font(family='Times', size=12)
+# pdf.cell(w=0, h=12, txt="Hello", align='L', ln=1, border=1)
+# pdf.set_font(family='Times', style='B', size=12)
+# pdf.cell(w=0, h=12, txt="hi there", align='L', ln=1, border=1)
 
 pdf.output("output.pdf")
